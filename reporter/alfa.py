747788785273fb8677f231adc92b5399931c38a7
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-10-06 17:46:42 +0300

    Codes changed

--- reporter/alfa.py
+++ reporter/alfa.py
@@ -4,12 +4,12 @@ from . import column_types as ct
 ATTACH_CONFIG = Config({
     "codes": (
         ct.Codes(  # KDC
-            clinic_code=ct.ClinicCode(value='AO “АльфаСтрахование” КДЦ'),
+            clinic_code=ct.ClinicCode(value='АО "АльфаСтрахование" КДЦ'),
             control_code=ct.ControlCode(value='0016/СК'),
             medicine_id=ct.MedicinesID(value=921)
         ),
         ct.Codes(  # PK
-            clinic_code=ct.ClinicCode(value='AO “АльфаСтрахование” ПК'),
+            clinic_code=ct.ClinicCode(value='АО "АльфаСтрахование" ПК'),
             control_code=ct.ControlCode(value='980/24/10-15'),
             medicine_id=ct.MedicinesID(value=921)
         )
