b11d34c7834db1a8ca4870efbc6dbdd4fa2dd89e
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-11-03 19:18:23 +0300

    Somthing new

---
+++ ui/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {},
+    colors: {},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
