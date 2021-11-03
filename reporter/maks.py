92978c67d4a62f22dc378440231488782e8e9b89
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2021-11-03 19:23:31 +0300

    Max changed

--- reporter/maks.py
+++ reporter/maks.py
@@ -10,12 +10,8 @@ class MaksAttach(MaksReport):
     def get_data(self):
         data = []
         for row in self.ws.iter_rows(min_row=2, values_only=True):
-            if 'судебный' in row[1].lower().split(" "):
-                codes = [["МАКС КДЦ Судебный департамент", "0034/СК Судебный департамент", 4001477],
-                         ["МАКС ПК Судебный департамент", "(МАКС) 101725/16-57937/330/2017 Судебный департамент", 4001477]]
-            else:
-                codes = [["МАКС КДЦ", "0034/СК", 4001477],
-                         ["Макс стандарт", "(МАКС) 101725/16-57937/330/2017", 4001477]]
+            codes = [["СУДЕБНЫЙ КОРПУС МАКС РК", "Судебный корпус МАКС РК", 4001477],
+                     ["СУДЕБНЫЙ КОРПУС МАКС ПК", "Судебный корпус МАКС ПК", 4001477]]
             for code in codes:
                 data += [[row[4], row[5], row[6], row[8], row[3], row[9], row[10], None] + code]
         return data
@@ -25,12 +21,8 @@ class MaksDetach(MaksReport):
     def get_data(self):
         data = []
         for row in self.ws.iter_rows(min_row=2, values_only=True):
-            if 'судебный' in row[1].lower().split(" "):
-                codes = [["МАКС КДЦ Судебный департамент", "0034/СК Судебный департамент", 2],
-                         ["МАКС ПК Судебный департамент", "(МАКС) 101725/16-57937/330/2017 Судебный департамент", 2]]
-            else:
-                codes = [["МАКС КДЦ", "0034/СК", 4001477],
-                         ["Макс стандарт", "(МАКС) 101725/16-57937/330/2017", 4001477]]
+            codes = [["СУДЕБНЫЙ КОРПУС МАКС РК", "Судебный корпус МАКС РК", 2],
+                     ["СУДЕБНЫЙ КОРПУС МАКС ПК", "Судебный корпус МАКС ПК", 2]]
             for code in codes:
                 data += [[row[4], row[5], row[6], row[8], row[3], None, None, row[10]] + code]
         return data
@@ -45,7 +37,9 @@ def get_maks(file, attach=False, detach=False):
 
 
 if __name__ == '__main__':
-    test_attach = MaksAttach("/Users/grigorijhanin/Documents/Работа пупс/PupsApp/test/Макс/0501p210315_05322_210312.xls")
-    test_detach = MaksDetach("/Users/grigorijhanin/Documents/Работа пупс/PupsApp/test/Макс/0501p210315_05322_210312.xls")
+    test_attach = MaksAttach(
+        "/Users/grigorijhanin/Documents/Работа пупс/PupsApp/test/Макс/0501p210315_05322_210312.xls")
+    test_detach = MaksDetach(
+        "/Users/grigorijhanin/Documents/Работа пупс/PupsApp/test/Макс/0501p210315_05322_210312.xls")
     test_attach.get_data()
     test_detach.get_data()
