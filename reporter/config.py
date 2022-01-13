8a06c947229d331b7d59291ac9954ca6999ae6c3
Grigoriy Hanin <43445998+haningrisha@users.noreply.github.com>
2022-01-13 21:01:51 +0300

    Renessans code changed

--- reporter/config.py
+++ reporter/config.py
@@ -1,7 +1,7 @@
 header = ["SURNAME", "FIRST_NAME", "SEC_NAME", "DATE_BIRTH", "POLICY", "DATE_FRM", "DATE_TO", "DATE_CNCL",
           "CLINK_CODE", "CONTR_CODE", "MEDECINS_ID"]
 codes = {
-    "renessans1": ["ПРЯМОЙ ДОСТУП ПК", "136 ПРЯМОЙ ДОСТУП ПК"],
+    "renessans1": ["Ренессанс ПРЯМОЙ ДОСТУП ПК", "136 ПРЯМОЙ ДОСТУП ПК"],
     "renessans2": ["Ренессанс КДЦ прямой доступ", "0010/СК ПРЯМОЙ ДОСТУП КДЦ"],
     "alyans1": ["АЛЬЯНС ПРЯМОЙ ПК", "700 ДС11 ПРЯМОЙ"],
     "alyans2": ["АЛЬЯНС ПРЯМОЙ КДЦ", "0005/СК ДС4 ПРЯМОЙ"],
